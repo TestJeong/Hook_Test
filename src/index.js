36b6d5f93a95c67769161e084505797959a1a919
TestJeong <jyunj3015@gmail.com>
2020-08-14 18:07:43 +0900

    Login Page 구현 1

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const createStoreWithMiddleware = applyMiddleware(
+  promiseMiddleware,
+  ReduxThunk
+)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(Reducer)}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
